80e368f3d6dcd2f2407eb21ff11332c6e8236afb
Dogma11 <graverondu11@gmail.com>
2020-10-11 22:27:46 +0200

    prepare for player timelimit imp

--- src/MemeView.js
+++ src/MemeView.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import MemePlayer from './MemePlayer.js';
-import './App.css';
+import React from "react";
+import MemePlayer from "./MemePlayer.js";
+import "./App.css";
 
 class MemeView extends React.Component {
-    render() {
-      return (
-        <div className="shopping-list">
-          <h1>Meme List for this {this.props.day}</h1>
-          <ul>
-            <MemePlayer name="RICARDO" url="https://www.youtube.com/watch?v=BsFu9oMFrJU"/>
-            <MemePlayer name="EVO TAXIS" url="https://www.youtube.com/watch?v=0kDKycNbopM"/>
-            <MemePlayer name="DAVID ENOUGH" url="https://www.youtube.com/watch?v=ho4W5LnFl6s"/>
-          </ul>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Meme List for this {this.props.day}</h1>
+        <ul>
+          <MemePlayer
+            name="RICARDO"
+            url="https://www.youtube.com/watch?v=O_IYLqIjtMg"
+          />
+          <MemePlayer
+            name="EVO TAXIS"
+            url="https://www.youtube.com/watch?v=0kDKycNbopM"
+          />
+          <MemePlayer
+            name="DAVID GOOD ENOUGH"
+            url="https://www.youtube.com/watch?v=ho4W5LnFl6s"
+          />
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default MemeView
+export default MemeView;
